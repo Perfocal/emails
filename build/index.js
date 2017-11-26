23fd0ac5a7b1b03f8089183445aad712f72d7c80
Scott Wang <me@scottoas.is>
2017-11-26 21:59:35 +0800

    Rename index.js to build/index.js

--- build/index.js
+++ build/index.js
@@ -2,13 +2,13 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const fs = require('fs');
-const PAGE_ROOT = "views";
+const PAGE_ROOT = "../views";
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, PAGE_ROOT));
 
 (function addListener (pathname) {
-  const fullPath = PAGE_ROOT + pathname;
+  const fullPath = path.join(__dirname, PAGE_ROOT, pathname);
   fs.readdir(fullPath, (_, items) => {
     items.forEach((item) => {
       fs.stat(`${fullPath}/${item}`, (_, stat) => {
@@ -28,7 +28,7 @@ app.set('views', path.join(__dirname, PAGE_ROOT));
 })('');
 
 // declare static directory
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../public')));
 
 app.listen(3002);
 console.log('Now the ejs app is un and running at http://localhost:3002/');
