7e8d082a5ee6c1494840cde1a05e010be9617247
a-vorontsov <vorontsov98@gmail.com>
2017-11-28 18:56:30 +0000

    Edit connect variables, Add placeholder data, Add booking-cancelled and shoot-reminder

--- build/index.js
+++ build/index.js
@@ -3,23 +3,40 @@ const app = express();
 const path = require('path');
 const fs = require('fs');
 const PAGE_ROOT = "..";
+const photog = require('../data/photog.json');
+const event = require('../data/event.json');
+const user = require('../data/user.json');
+const subject = require('../data/subject.json');
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, PAGE_ROOT));
 
-(function addListener (pathname) {
+(function injectData (pathname){
   const fullPath = path.join(__dirname, PAGE_ROOT, pathname);
   fs.readdir(fullPath, (_, items) => {
     items.forEach((item) => {
       fs.stat(`${fullPath}/${item}`, (_, stat) => {
         const path = `${pathname}/${item}`;
         if (stat.isDirectory()) {
-          addListener(path);
-        } else {
-          const realpath = path.substr(0, path.length - 4);
-          app.get(realpath, (req, res) => res.render(path.substr(1)));
+          switch(path){
+            case "/":
+            case "/parts":
+            case "/customer":
+            case "/photographer":
+            injectData(path);
+          }
+        } else if (path.endsWith('.ejs')) {
+          let realpath = path.substr(0, path.length - 4);
+          var label = JSON.stringify(realpath);
+          bodyData = {
+            subject: subject[realpath],
+            photog: photog,
+            event: event,
+            user: user
+          };
+          app.get(realpath, (req, res) => res.render(path.substr(1), bodyData));
           if (/index.ejs$/.test(item)) {
-            app.get(pathname, (req, res) => res.render(path.substr(1)));
+            app.get(pathname, (req, res) => res.render(path.substr(1), bodyData));
           }
         }
       });
